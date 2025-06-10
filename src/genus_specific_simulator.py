cd7b77f07ffa61fec299a059d1eda44851d0f3ed
Thomas McKeown <tmckeown@sandiego.edu>
2025-06-10 14:15:15 -0700

    Initial commit for adding model loader for the specific genus models

--- src/genus_specific_simulator.py
+++ src/genus_specific_simulator.py
@@ -18,3 +18,17 @@ if __name__ == '__main__':
 
     genus_specific_tp = GenusSpecificModelTrainer(df)
     genus_specific_tp.train_genus("Specularius", 5)
+
+    # Load Genus models
+    genus_model_paths = {
+            "caud" : globals.gen_caud_model, 
+            "dors" : globals.gen_dors_model,
+            "fron" : globals.gen_fron_model,
+            "late" : globals.gen_late_model
+        }
+    
+    GENUS_OUTPUTS = dbr.get_num_genus()
+    genus_ml = ModelLoader(genus_model_paths, GENUS_OUTPUTS)
+    genus_models = genus_ml.get_models()
+
+    cal_model = genus_ml.load_genus_specific_model("Callosobruchus")
