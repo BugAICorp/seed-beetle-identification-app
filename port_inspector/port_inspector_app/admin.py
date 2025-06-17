6dd77ebc6d00cf3aa0f179dcdcb1cb848539c4b7
Thomas McKeown <tmckeown@sandiego.edu>
2025-06-17 13:14:01 -0700

    made migrations for the database

--- port_inspector/port_inspector_app/admin.py
+++ port_inspector/port_inspector_app/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import SpecimenUpload
-from .models import KnownSpecies
-from .models import Genus
+from .models import SpecimenUpload, KnownSpecies, Genus, TrainingDatabase
 
+admin.site.register(TrainingDatabase)
 admin.site.register(SpecimenUpload)
 admin.site.register(KnownSpecies)
 admin.site.register(Genus)
