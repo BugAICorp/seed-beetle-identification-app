e40a2682c79e1113fa5ffb1abd10d608c3e03973
Thomas McKeown <tmckeown@sandiego.edu>
2025-06-30 11:25:39 -0700

    Changed ood temps to 1 until a better solution is found for evaluating the model's confidence

--- port_inspector/beetle_detection/evaluation_method.py
+++ port_inspector/beetle_detection/evaluation_method.py
@@ -119,7 +119,7 @@ class EvaluationMethod:
 
                 # Apply OOD for out-of-distribution detection
                 # Threshold to be adjusted (If threshold is too strict (try −14) If too lenient (try −10))
-                is_confident, energy, softmax_scores = self.apply_ood(model_output, temperature=1000, threshold=-12.0)
+                is_confident, energy, softmax_scores = self.apply_ood(model_output, temperature=1, threshold=-12.0)
 
                 if not is_confident:
                     # Get the predicted top 5 species(or less if not enough outputs) and their indices
--- port_inspector/beetle_detection/genus_evaluation_method.py
+++ port_inspector/beetle_detection/genus_evaluation_method.py
@@ -123,7 +123,7 @@ class GenusEvaluationMethod:
 
                 # Apply OOD for out-of-distribution detection
                 # Threshold to be adjusted (If threshold is too strict (try −14) If too lenient (try −10))
-                is_confident, energy, softmax_scores = self.apply_ood(model_output, temperature=1000, threshold=-12.0)
+                is_confident, energy, softmax_scores = self.apply_ood(model_output, temperature=1, threshold=-12.0)
 
                 if is_confident:
                     # Use the predicted class and softmax confidence
