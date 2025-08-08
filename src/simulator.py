8ef3cdd6d457c72a20bbcc7ab7d486610d5120b5
Joe Cox <joecox@sandiego.edu>
2025-08-08 13:49:57 -0600

    Fixed naming errors

--- src/simulator.py
+++ src/simulator.py
@@ -178,7 +178,8 @@ if __name__ == '__main__':
 
     # Training
     if train_caud:
-        species_tp.train_resnet_model(20, "caud", batch=64, rotation=9, brightness=0.18230462, lrate=0.0003845612)
+        species_tp.train_resnet_model(
+            20, "caud", batch=64, rotation=9, brightness=0.18230462, lrate=0.0003845612)
         if show_plots:
             species_tp.analyze_f1_scores_single_split(
                 "caud", batch_size=32, plot_save_path="species_caud_plot.png", plot=True)
@@ -187,17 +188,19 @@ if __name__ == '__main__':
             20, "dors", batch=16, rotation=2, brightness=0.2288617393, lrate=0.00017452)
         if show_plots:
             species_tp.analyze_f1_scores_single_split(
-                "caud", batch_size=32, plot_save_path="species_genus_caud_plot.png", plot=True)
+                "dors", batch_size=32, plot_save_path="species_dors_plot.png", plot=True)
     if train_fron:
-        species_tp.train_resnet_model(20, "fron", batch=32, rotation=0, brightness=0.110488612, lrate=0.0002088527)
+        species_tp.train_resnet_model(
+            20, "fron", batch=32, rotation=0, brightness=0.110488612, lrate=0.0002088527)
         if show_plots:
             species_tp.analyze_f1_scores_single_split(
-                "caud", batch_size=32, plot_save_path="species_genus_caud_plot.png", plot=True)
+                "fron", batch_size=32, plot_save_path="species_fron_plot.png", plot=True)
     if train_late:
-        species_tp.train_resnet_model(20, "late", batch=16, rotation=4, brightness=0.17189646, lrate=0.00007408262)
+        species_tp.train_resnet_model(
+            20, "late", batch=16, rotation=4, brightness=0.17189646, lrate=0.00007408262)
         if show_plots:
             species_tp.analyze_f1_scores_single_split(
-                "caud", batch_size=32, plot_save_path="species_genus_caud_plot.png", plot=True)
+                "late", batch_size=32, plot_save_path="species_late_plot.png", plot=True)
 
     # Save models
     species_model_filenames = {
@@ -219,25 +222,29 @@ if __name__ == '__main__':
 
     # Training
     if train_caud:
-        genus_tp.train_resnet_model(20, "caud", batch=32, rotation=12, brightness=0.153764767, lrate=0.000197477148)
+        genus_tp.train_resnet_model(
+            20, "caud", batch=32, rotation=12, brightness=0.153764767, lrate=0.000197477148)
         if show_plots:
             genus_tp.analyze_f1_scores_single_split(
                 "caud", batch_size=32, plot_save_path="genus_caud_plot.png", plot=True)
 
     if train_dors:
-        genus_tp.train_resnet_model(20, "dors", batch=16, rotation=8, brightness=0.150820822, lrate=0.000216600199)
+        genus_tp.train_resnet_model(
+            20, "dors", batch=16, rotation=8, brightness=0.150820822, lrate=0.000216600199)
         if show_plots:
             genus_tp.analyze_f1_scores_single_split(
                 "dors", batch_size=16, plot_save_path="genus_dors_plot.png", plot=True)
 
     if train_fron:
-        genus_tp.train_resnet_model(20, "fron", batch=16, rotation=2, brightness=0.20239523572, lrate=0.0001681036183)
+        genus_tp.train_resnet_model(
+            20, "fron", batch=16, rotation=2, brightness=0.20239523572, lrate=0.0001681036183)
         if show_plots:
             genus_tp.analyze_f1_scores_single_split(
                 "fron", batch_size=16, plot_save_path="genus_fron_plot.png", plot=True)
 
     if train_late:
-        genus_tp.train_resnet_model(20, "late", batch=32, rotation=13, brightness=0.24352227695, lrate=0.0001241454983)
+        genus_tp.train_resnet_model(
+            20, "late", batch=32, rotation=13, brightness=0.24352227695, lrate=0.0001241454983)
         if show_plots:
             genus_tp.analyze_f1_scores_single_split(
                 "late", batch_size=32, plot_save_path="genus_late_plot.png", plot=True)
